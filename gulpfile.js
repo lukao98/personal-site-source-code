3535ee2ad4a68c6aabe12927acea50c6925d8c98
Lucas <luckagabriel@hotmail.com>
2016-10-28 14:58:14 -0200

    Erros corrigidos

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,35 @@
 var stylus = require('gulp-stylus'),
     pug = require('gulp-pug'),
-    gulp = require('gulp');
+    gulp = require('gulp'),
+    clean = require('gulp-clean'),
+    plumber = require('gulp-plumber');
     
 
-    gulp.task('stylus', function()){
-    		return gulp.src('./src/styles/main.styl')
+    gulp.task('clean', function(){
+    	return gulp.src('./app/', { read : false })
+    	.pipe(clean({ force : true }));
+
+    });
+    
+    gulp.task('stylus', ['clean'], function(){
+    	gulp
+    		.src('./src/styles/main.styl')
+    		.pipe(plumber())
     		.pipe(stylus())
-    		.pipe(gulp.dest(app/assets/css))
+    		.pipe(gulp.dest('app/assets/css'))
     });
 
-     gulp.task('pug', function()){
-    		return gulp.src('./src/views/index.pug')
+     gulp.task('pug', ['clean'], function(){
+    	gulp	
+    		.src('./src/views/index.pug')
+    		.pipe(plumber())
+            .pipe(pug({ pretty : true }))
     		.pipe(pug())
+    		.pipe(gulp.dest('./app/'));
     });
 
-    gulp.task('build', function()){
-    	gulp.run('stylus');
-    	gulp.run('pug');
+    gulp.task('build', function(){	
+    	gulp.watch('stylus');
+    	gulp.watch('pug');
     });
 
