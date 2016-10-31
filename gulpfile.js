15ad6486599654c428e286510f101bb6ddbc86d8
Lucas Gabriel <luckagabriel@hotmail.com>
2016-10-31 10:48:55 -0200

    Adicionado tasks e dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,5 @@
-var stylus = require('gulp-stylus'),
-    pug = require('gulp-pug'),
-    gulp = require('gulp'),
-    clean = require('gulp-clean'),
-    plumber = require('gulp-plumber'),
-    taskList = ['clean', 'stylus', 'pug'];
-    
-
-    gulp.task('clean', function(){
-    	return gulp.src('./app/', { read : false })
-    	.pipe(clean({ force : true }));
-
-    });
-    
-    gulp.task('stylus', function(){
-    	gulp
-    		.src('./src/styles/main.styl')
-    		.pipe(plumber())
-    		.pipe(stylus())
-    		.pipe(gulp.dest('./app/assets/css'))
-    });
-
-     gulp.task('pug', function(){
-    	gulp	
-    		.src('./src/views/index.pug')
-    		.pipe(plumber())
-            .pipe(pug({ pretty : true }))
-    		.pipe(pug())
-    		.pipe(gulp.dest('./app/'));
-    });
-
-    gulp.task('build', taskList, function(){	
-    });
+require('./tasks/dev/serve.js');
+require('./tasks/dev/clean.js');
+require('./tasks/dev/templates.js');
+require('./tasks/dev/styles.js');
 
