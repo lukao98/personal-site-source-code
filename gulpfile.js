bd9f7ce7c1b17034d4fc870e554b5017fd04d534
Lucas <luckagabriel@hotmail.com>
2016-11-02 03:31:22 -0200

    Template-watch restaurada

--- gulpfile.js
+++ gulpfile.js
@@ -2,4 +2,5 @@ require('./tasks/dev/serve.js');
 require('./tasks/dev/clean.js');
 require('./tasks/dev/templates.js');
 require('./tasks/dev/styles.js');
+require('./tasks/dev/template-watch.js');
 
