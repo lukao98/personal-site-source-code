1e8639d49ea11a729e45d4b8b72e1ecab89de3cf
Lucas <luckagabriel@hotmail.com>
2016-11-07 15:10:42 -0200

    Wendell toc corrigido

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ require('./tasks/dev/clean.js');
 require('./tasks/dev/templates.js');
 require('./tasks/dev/styles.js');
 require('./tasks/dev/template-watch.js');
-require('./tasks/dev/imagens.js');
+require('./tasks/dev/images.js');
 require('./tasks/dev/js.js');
 
 //tasks prod
