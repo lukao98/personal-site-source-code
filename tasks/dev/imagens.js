19a045a2174c47fb344a11d887a03a68a4a4fc71
Lucas <luckagabriel@hotmail.com>
2016-11-07 14:43:56 -0200

    alterações e bug cannot

--- tasks/dev/imagens.js
+++ tasks/dev/imagens.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 
-gulp.task('imagens', ['clean'], function() {
+gulp.task('images', ['clean'], function() {
   gulp.src('./src/img/**/*.{jpg,png,gif}')
   .pipe(gulp.dest('./app/assets/img'));
 });
--- tasks/dev/serve.js
+++ tasks/dev/serve.js
@@ -1,11 +1,11 @@
-var taskList = ['clean', 'templates', 'styles', 'imagens', 'js'],
+var taskList = ['clean','js', 'styles', 'images', 'template-watch'],
 	gulp = require('gulp'),
 	browserSync = require('browser-sync');
 
  gulp.task('serve',taskList, function(){
  	 browserSync({server: './app'});
 
- 	 	gulp.watch('./src/styl/**/*.styl', ['styles']);
+ 	 	gulp.watch('./src/styles/**/*.styl', ['styles']);
  	 	gulp.watch('./src/views/**/*.pug', ['templates']);
  	 	gulp.watch('./src/js/**/*.js', ['js']);
  	 	gulp.watch('./src/img/**/*.{jpg,png,gif}', ['imagens']);
