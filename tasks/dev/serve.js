9e88e2b90f04cbe319fd5b457b860e381b6340b4
Lucas <luckagabriel@hotmail.com>
2016-11-08 12:32:36 -0200

    browser-sync corrigido

--- tasks/dev/serve.js
+++ tasks/dev/serve.js
@@ -1,4 +1,4 @@
-var taskList = ['clean','js', 'styles', 'images', 'template-watch'],
+var taskList = ['clean','js', 'styles', 'images','templates', 'template-watch'],
 	gulp = require('gulp'),
 	browserSync = require('browser-sync');
 
@@ -6,7 +6,7 @@ var taskList = ['clean','js', 'styles', 'images', 'template-watch'],
  	 browserSync({server: './app'});
 
  	 	gulp.watch('./src/styles/**/*.styl', ['styles']);
- 	 	gulp.watch('./src/views/**/*.pug', ['templates']);
+ 	 	gulp.watch('./src/views/**/*.pug', ['template-watch']);
  	 	gulp.watch('./src/js/**/*.js', ['js']);
  	 	gulp.watch('./src/img/**/*.{jpg,png,gif}', ['imagens']);
  });
--- tasks/dev/styles.js
+++ tasks/dev/styles.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp'),
 	stylus = require('gulp-stylus'),
-	plumber = require('gulp-plumber');
+	plumber = require('gulp-plumber'),
+	browserSync = require('browser-sync');
 
  gulp.task('styles', function(){
     gulp
     	.src('./src/styles/main.styl')
     	.pipe(plumber())
     	.pipe(stylus())
-    	.pipe(gulp.dest('./app/assets/css'));
+    	.pipe(gulp.dest('./app/assets/css'))
+    	.pipe(browserSync.reload({stream:true}));
  });
